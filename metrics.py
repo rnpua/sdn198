6bc7363bfcc66f7347fd6d78b26d87526074aa18
rnpua <rnpua621@gmail.com>
2017-04-23 11:13:03 -0700

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -8,35 +8,52 @@ import time
 import networkx as nx
 from sys import exit
 
-route = {}
-cost = 0
-finalLinkTX = {}
-prevswitch = ""
-switch = ""
+global finalLinkTX
 
-route = ""
+def get_hopcount():
+	#route = []
+	global cost
+	global finalLinkTX
+	cost = 0
+	finalLinkTX = {}
+	prevswitch = ""
+	switch = ""
+	switchtmp = ""
+	route = ""
+	src = "00:00:00:00:00:00:00:03"
+	dst = "00:00:00:00:00:00:00:01"
 
-metric = "http://localhost:8080/wm/routing/paths/slow/00:00:00:00:00:00:00:01/00:00:00:00:00:00:00:02/5/json"
-data = requests.get(metric)
-jdata = json.loads(data.content)
-src = '00:00:00:00:00:00:00:02'
-#dst = jdata ['src_dpid']
-srcShortID = src.split(":")[7]
+	metric = "http://localhost:8080/wm/routing/paths/slow/00:00:00:00:00:00:00:03/00:00:00:00:00:00:00:01/10/json"
+	data = requests.get(metric)
+	jdata = json.loads(data.content)
+	
+	#dst = jdata ['src_dpid']
+	srcShortID = src.split(":")[7]
+	content = data.content
 
-#print jdata
-for i in jdata['results']:
-	route = srcShortID
-	if (i['hop_count']):
-		cost = i['hop_count']
-		for key in i['path']:
-			switch = key.split(":")[7]
-			if (srcShortID == switch):
-				continue
-			elif(prevswitch == switch):
-				continue
-			else:
-				route = route + "::" + switch 
-			prevswitch = switch 
+	#print data.content
+	#print jdata[0]
+	#print content
+	#print jdata
+	for i in jdata['results']:
+		src = i['src_dpid'].encode('ascii','ignore')
+		route = i['src_dpid'].split(":")[7]
+		cost = (int)(i['hop_count'])
+		#print cost
+	for j in i['path']:
+		#switch = j
+		switch = j['dpid']
+		if switch == prevswitch:
+			continue
+		elif switch == src:
+			continue
+		else:
+			switchtmp = switch.split(":")[7]
+			route = route + "::" + switchtmp
+			prevswitch = switch
+#	print route
 	finalLinkTX[route] = cost
-			 
+		
+	
+get_hopcount()
 print finalLinkTX
